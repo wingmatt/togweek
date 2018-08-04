701268ab9dcb4c72a4ee9546189ef7b29530abc4
Matt Wing <20592216+wingmatt@users.noreply.github.com>
2018-08-04 16:52:04 -0700

    Not working yet for the checkbox effect

--- interface.js
+++ interface.js
@@ -6,3 +6,13 @@ $(".toggl-stop").click(function(){
 	$(this).toggle(false);
 	$(this).siblings(".toggl-start").toggle(true);
 });
+$(".todo").click(function(){
+	console.log($(this).find("[type=checkbox]").checked);
+	if ($(this.find("[type=checkbox]").checked)) {
+		$(this).addClass("done");
+		console.log('DONE');
+	} else {
+		$(this).removeClass("done");
+		console.log('UNDONE');
+	}
+});
