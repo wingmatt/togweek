a92352dc09d8540f0763393c39784eb5c5694d2b
Matt Wing <20592216+wingmatt@users.noreply.github.com>
2018-08-04 23:16:57 -0700

    Do things

--- functions.js
+++ functions.js
@@ -3,35 +3,89 @@ $(document).ready(function(){
 	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 	var today  = new Date();
 	$("#time").text(today.toLocaleDateString("en-US", options));
+	//fetchTeamweek(auth);
 });
 // Get list of Teamweek tasks
 /*---
 https://teamweek.com/api/v4/1/tasks/timeline?since=&until=&users=&projects=&tasks=&group=
 ---*/
 function fetchTeamweek(auth){
-
+	var taskArray = [];
+	var today  = new Date();
+	var postUrl = "https://teamweek.com/api/v4/1/tasks/timeline?since=" + today + "&until" + today + "&users=ME";
+	var jqxhr = $.post( "example.php", function(key, value) {
+	  $(rawData).each(parseTeamweek(value));
+	  // TODO probs fix
+	  alert( "success" );
+	})
+	  .done(function() {
+	    alert( "second success" );
+	  })
+	  .fail(function() {
+	    alert( "error" );
+	  })
+	  .always(function() {
+	    alert( "finished" );
+	  });
+	 
+	// Perform other work here ...
+	 
+	// Set another completion function for the request above
+	jqxhr.always(function() {
+	  alert( "second finished" );
+	});
 }
 
-/* --REFERENCE--
-// Assign handlers immediately after making the request,
-// and remember the jqxhr object for this request
-var jqxhr = $.post( "example.php", function() {
-  alert( "success" );
-})
-  .done(function() {
-    alert( "second success" );
-  })
-  .fail(function() {
-    alert( "error" );
-  })
-  .always(function() {
-    alert( "finished" );
-  });
- 
-// Perform other work here ...
- 
-// Set another completion function for the request above
-jqxhr.always(function() {
-  alert( "second finished" );
-});
-*/
+
+
+
+/* Example response format---------------------------/
+
+[
+  {
+    "id": 1,
+    "name": "Act like muppet",
+    "notes": "must try hard",
+    "start_date": "2016-01-01",
+    "end_date": "2016-01-01",
+    "start_time": "12:00",
+    "end_time": "13:00",
+    "color": "#db9a9a",
+    "color_id": 1,
+    "estimated_hours": "3.5",
+    "estimated_minutes": "210",
+    "done": false,
+    "user_id": 1,
+    "folder_id": 1,
+    "position": 1,
+    "weight": 1,
+    "project": {
+      "id": 1,
+      "name": "Quality time",
+      "archived": false,
+      "color": "#db9a9a",
+      "color_id": 1,
+      "created_at": "2016-01-01T00:00:00.00Z+00:00",
+      "updated_at": "2016-01-01T00:00:00.00Z+00:00"
+    },
+    "created_at": "2016-01-01T00:00:00.00Z+00:00",
+    "updated_at": "2016-01-01T00:00:00.00Z+00:00"
+  }
+]
+
+----------------------------------------------------*/
+
+// Format JSON response. Hoo boy
+function parseTeamweek (i, rawData) {
+	var name = rawData.name;
+	var notes = rawData.notes;
+	var color = rawData.color;
+	var projName = rawData.project.name;
+	var projColor = rawData.project.color;
+	var done = rawData.project.done;
+	var user = rawData.user_id;
+}
+
+// Save entered API keys locally
+
+// Load saved API keys on pageload
