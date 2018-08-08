be9ae13d9073ff3816000e906540bb0abede4be2
wingmatt <mattw@dotcomjungle.com>
2018-08-08 12:40:14 -0700

    Add JSON parse to LS from other project

---
+++ js/localStorage.js
@@ -0,0 +1,23 @@
+//Reuse JSON parse and saving
+function processData() {
+    // Get stored data from failed sends, if it exists
+    var savedData = JSON.parse(localStorage.getItem("tw_data") || null) || [];
+    console.log('Saved Data Before:' + savedData);
+
+    // Check if there's an email address to send current form data to
+    var emailField = document.getElementById("emailAddress");
+    var isEmailValid = emailField.checkValidity();
+    if (isEmailValid && emailField.value) {
+        // Get current values and assemble into JSON
+            // TODO Fetch TW JSON response here!
+        // Add current data to stored data.
+        savedData.push(formJson);
+        console.log('Saved Data After:' + savedData);
+        // Get JSON ready for storage.
+        var combinedData = JSON.stringify(savedData);
+        console.log('Combined Data Var:' + combinedData);
+        // Store stringified JSON.
+        localStorage.setItem("tw_data", combinedData);
+        console.log('Combined Data After:' + localStorage.getItem("tw_data"));
+    }
+}
